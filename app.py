457fe9bd14b4eba6442fc6c3224d4353cf6b01af
LorenzoMonti <lorenzomonti42@gmail.com>
2019-12-04 06:45:58 +0100

    modified app.py in order to work with Raspberry Pi

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, abort, make_response
+import csv
 
 #Create a flask object
 app = Flask(__name__)
@@ -11,35 +12,18 @@ def not_found(error):
 #Run the code below this function when someone access to this URL of the server
 @app.route('/api/v1.0/getRange/<int:timestamp_from>/<int:timestamp_to>', methods=['GET'])
 def get_range(timestamp_from, timestamp_to):
-
+    #print("richiesta:", timestamp_from, timestamp_to)
     json = []
+    with open("../counting_people/letture.csv") as csvfile:
+        csvreader = list(csv.reader(csvfile, delimiter=',', quotechar='|'))
+        for row in csvreader:
+            if(int(row[0])>= timestamp_from and int(row[0]) <= timestamp_to):
+               json.append({'timestamp':row[0], 'students':row[1]})
 
-    #TODO inserisco nell'array json tutti i valori del file csv corrispondenti al range di input
-    for i in range(0, 10):
-        element = {
-            'timestamp': 'timespamp csv',
-            'students' : 'students csv'
-        }
-        json.append(element)
-    
     if len(json) == 0:
         abort(404)
     return jsonify({'json': json})
 
 #server listen on the port 80 and return any errors
 if __name__ == '__main__':
-    app.run(host='192.168.1.100', port = 80, debug = True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+    app.run(host='0.0.0.0', port = 80, debug = True)
